a41533ae3eebf5d55553f20e69ab8677576f2844
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-07-02 18:40:42 +0300

    fix: chromium images width

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Image from "next/image";
 
 const Card = () => {
     const [currentPage, setCurrentPage] = useState(1);
@@ -13,10 +14,10 @@ const Card = () => {
     };
 
     const images = [
-        'mk2.jpg',
-        'mk1.jpg',
-        'mk3.webp',
-        'map.png',
+        '/mk2.jpg',
+        '/mk1.jpg',
+        '/mk3.webp',
+        '/map.png',
 
     ];
     const [selectedImage, setSelectedImage] = useState(images[0]);
@@ -30,7 +31,8 @@ const Card = () => {
         <div className="flex h-96 mb-4">
             <div className="w-2/3 mx-10">
                 {selectedImage ? (
-                    <img src={selectedImage} alt="Selected" className="w-full h-full object-cover rounded-lg"/>
+                    <Image src={selectedImage} width={500}
+                           height={500} alt="Selected" className={`w-full h-full object-cover rounded-lg`}/>
                 ) : (
                     <div className="w-full h-64 bg-gray-200 flex items-center justify-center">
                         Select an image
@@ -42,15 +44,17 @@ const Card = () => {
                     {currentImages.map((image) => (
                         <div
                             key={image}
-                            className="h-1/4 p-2"
+                            className="h-1/4 w-full p-2"
                             onClick={() => handleImageSelect(image)}
                         >
-                            <img
+                            <Image
                                 src={image}
                                 alt="Thumbnail"
                                 className={`w-full h-full object-cover rounded-lg ${
                                     selectedImage === image ? 'border-4 border-orange-400' : ''
                                 }`}
+                                width={500}
+                                height={500}
                             />
                         </div>
                     ))}
